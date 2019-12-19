e6f26b11693bfcfb46f52600249ebdf5b4fc2679
Pedro Pérez <e-pdperez@neoris.com>
2019-12-19 13:42:41 -0600

    progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// const Hello = (props) => <h2>{props.title}</h2>
+
+class Hello extends Component {
+  render() {
+    return <h2>{this.props.title}</h2>
+  }
+}
+
+class Text extends Component {
+  render() {
+    const isActivated = this.props.isActivaded ? 'On' : 'Off'
+    const mappedArray = this.props.array.map(n => n*2)
+    return <div>
+      <p>{this.props.text}</p>
+      <p>{this.props.number}</p>
+      <p>{isActivated}</p>
+      <p>{mappedArray.join(', ')}</p>
+      <p>{this.props.object.key}</p>
+    </div>
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello title="Hello from props"/>
+        <Text 
+        array= {[1,2,3]}
+        object = {{key:'First value', key2:'Second value'}}
+        number={1} 
+        isActivated
+        text='Hi'/>
       </header>
     </div>
   );
